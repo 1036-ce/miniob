f1c89c1da78f1a22b0fd7f121b334fb0692df53e
1036-ce <2376030467@qq.com>
2025-08-16 21:47:39 +0800

    fix text-5

--- src/observer/common/config.h
+++ src/observer/common/config.h
@@ -4,5 +4,5 @@
 
 constexpr static const char* NULL_BITMAP_FIELD_NAME = "__null_bitmap";
 constexpr static int NULL_BITMAP_FIELD_ID = (1 << 16);
-constexpr static int TEXT_MAX_SIZE = 256;
+constexpr static int TEXT_MAX_SIZE = 65535;
 constexpr static int INDEX_MAX_COLUMN_COUNT = 8;
