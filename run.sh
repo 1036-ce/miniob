f4dca1d36194117bfa6ada4c7945f9fcd835cc99
1036-ce <2376030467@qq.com>
2025-08-16 22:09:20 +0800

    finish text

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./build/bin/observer -f ./etc/observer.ini -P cli
+# ./build/bin/observer -f ./etc/observer.ini -P cli
+./build/bin/observer -f ./etc/observer.ini -P port
--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 
 cd test/case
 
-python3 ./miniob_test.py --test-cases=primary-drop-table,basic,primary-date,primary-pure-null,primary-group-by,primary-null,primary-update,primary-expression,primary-aggregation-func,primary-join-tables,dblab-hash-join,primary-simple-sub-query,primary-complex-sub-query,primary-order-by,primary-text,primary-multi-index,primary-aggregation-and-groupby,primary-new-join,primary-new-csq
-# python3 ./miniob_test.py --test-cases=primary-simple-sub-query,primary-complex-sub-query,primary-new-csq
+# python3 ./miniob_test.py --test-cases=primary-drop-table,basic,primary-date,primary-pure-null,primary-group-by,primary-null,primary-update,primary-expression,primary-aggregation-func,primary-join-tables,dblab-hash-join,primary-simple-sub-query,primary-complex-sub-query,primary-order-by,primary-text,primary-multi-index,primary-aggregation-and-groupby,primary-new-join,primary-new-csq
+python3 ./miniob_test.py --test-cases=primary-text
