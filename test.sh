2e6d4bbf1923c800254d0930d78f0d740f0bb282
1036-ce <2376030467@qq.com>
2025-08-31 21:25:50 +0800

    add vector-high-dim test

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 
 cd test/case
 
-python3 ./miniob_test.py --test-cases=primary-drop-table,basic,primary-date,primary-pure-null,primary-group-by,primary-null,primary-update,primary-expression,primary-aggregation-func,primary-join-tables,dblab-hash-join,primary-simple-sub-query,primary-complex-sub-query,primary-order-by,primary-text,primary-multi-index,primary-aggregation-and-groupby,primary-new-join,primary-new-csq,primary-like,primary-alias,primary-unique,primary-new-unique,primary-create-table-select,primary-update-select,view,vector-basic,vector-format
-# python3 ./miniob_test.py --test-cases=primary-alias
+python3 ./miniob_test.py --test-cases=primary-drop-table,basic,primary-date,primary-pure-null,primary-group-by,primary-null,primary-update,primary-expression,primary-aggregation-func,primary-join-tables,dblab-hash-join,primary-simple-sub-query,primary-complex-sub-query,primary-order-by,primary-text,primary-multi-index,primary-aggregation-and-groupby,primary-new-join,primary-new-csq,primary-like,primary-alias,primary-unique,primary-new-unique,primary-create-table-select,primary-update-select,view,vector-basic,vector-format,vector-high-dim
+# python3 ./miniob_test.py --test-cases=vector-high-dim
